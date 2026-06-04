eb4378ea1c967efa90ddcef53feab87cd2d7f96d
edv <eguilhermeleitedev81@gmail.com>
2026-06-03 23:23:09 -0300

    feat: cria guarda para impedir acesso direto à edição via url

---
+++ src/app/guards/auth-guard.ts
@@ -0,0 +1,18 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from '@angular/core';
+import {Auth} from '../service/auth';
+
+export const authGuard: CanActivateFn = (route, state) =>
+{
+  const auth = inject(Auth);
+  const router = inject(Router);
+
+  if (auth.isAuthenticated())
+  {
+    return true;
+  }
+
+  router.navigate(['/login']);
+
+  return false;
+};
